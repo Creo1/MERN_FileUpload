65158283d39af0e1ed737f19e1c62228f39097ea
Ajaypal Gangwar <ap.gangwar95@gmail.com>
2018-04-29 23:38:08 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,29 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
-import cookie from 'react-cookie';
 import routes from './routes';
 import reducers from './reducers/index';
-import { AUTH_USER } from './actions/types';
 
 
-// Import stylesheets
-// import './public/stylesheets/base.scss';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
-
-const token = cookie.load('token');
-
-if (token) {
-  // Update application state. User has token and is probably authenticated
-  store.dispatch({ type: AUTH_USER });
-
-  window.addEventListener('storage', function (event) {
-    console.log(event.key, event.newValue);
-  });
-}
-
 
 ReactDOM.render(
   <Provider store={store}>
